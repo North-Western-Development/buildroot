5fbc2d061d0b5879728fe1d776d46bf6d6278ff7
Bernd Kuhls <bernd@kuhls.net>
2024-12-14 14:03:52 +0100

    {linux, linux-headers}: bump 6.{6, 12}.x series
    Signed-off-by: Bernd Kuhls <bernd@kuhls.net>
    Signed-off-by: Julien Olivain <ju.o@free.fr>
    (cherry picked from commit bb654e5a3bdef3576e82bd27e0ef809afb1b10d9)
    [Peter: drop 6.12.x bump]
    Signed-off-by: Peter Korsgaard <peter@korsgaard.com>

---
+++ sedna-sysroot/sysroot/usr/include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/deck/devel/buildroot/output/build/libressl-3.9.2/include
+# Build directory: /home/deck/devel/buildroot/output/build/libressl-3.9.2/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
