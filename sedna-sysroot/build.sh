a4469cd859cfffde5d5ef277405b35294d76406b
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-02 02:44:14 +0000

    replace the headers thing with a sysroot thing
    easy cross compiling setup

---
+++ sedna-sysroot/build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd "$(realpath "${0%/*}")" || { printf '%s\n' "Failed to cd to the script's directory"; exit 1; }
+[ -d ../output/host ] || make -C ..
+
+mkdir -p sysroot/usr
+cp -a ../output/staging/usr/include sysroot/usr
+cp -a ../output/staging/usr/lib sysroot/usr
+cp -a ../output/staging/lib sysroot
+rm -f sysroot/lib/*.a sysroot/usr/lib/*.a
+tar --owner=0 --group=0 -cJf ../sysroot.tar.xz sysroot
