7c7f493897967e899a7e52bc4d46a1f6d14e07ba
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-03 17:09:23 +0000

    setup disk script

---
+++ board/sedna-riscv64/rootfs-overlay/sbin/setup-disk.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+if [ "$(id -u)" != 0 ]; then
+    printf 'This must be run as root.\n'
+    exit 1
+fi
+
+if [ -z "$1" ]; then
+    printf 'usage: %s <drive>\n' "${0##*/}"
+elif ! [ -f "$1" ]; then
+    printf 'Drive %s not found.\n' "$1"
+fi
+
+printf 'This will destroy all data on %s.\n' "$1"
+read -r -p "Are you sure? [y/N] " response
+
+case $response in
+    [Yy]) ;;
+    *) exit 1 ;;
+esac
+
+mkfs.ext2 "$1"
+tmpdir=$(mktemp)
+mkdir "$tmpdir/mnt"
+mount "$1" "$tmpdir/mnt"
+mkdir "$tmpdir/mnt/upper"
+mkdir "$tmpdir/mnt/work"
+
+printf 'A reboot is required to use this drive.'
+read -r -p "Reboot now? [Y/n] " response
+
+case $response in
+    [Nn]) exit 0 ;;
+    *) reboot ;;
+esac
