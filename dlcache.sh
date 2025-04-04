220d1703444d8cc9417187de4b4420f05ae9b2d5
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-04 16:54:17 -0400

    fix

--- dlcache.sh
+++ dlcache.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-ver=$(curl -s 'https://api.github.com/repos/${{ github.repository }}/tags' | jq -r '.[1].name') # get the second latest release number
-if [ "$ver" != "null" ] && wget 'https://github.com/${{ github.repository }}'/releases/download/"$ver"/cache.tar.zst; then
+if [ -z "$GITHUB_REPOSITORY" ]; then
+    printf 'This should only be run as part of a GitHub actions workflow.\n'
+    exit 1
+fi
+
+ver=$(curl -s "https://api.github.com/repos/$GITHUB_REPOSITORY/tags" | jq -r '.[1].name') # get the second latest release number
+if [ "$ver" != "null" ] && wget "https://github.com/$GITHUB_REPOSITORY/releases/download/$ver/cache.tar.zst"; then
     tar -C ~ -xf cache.tar.zst
     mv ~/dl .
 fi
