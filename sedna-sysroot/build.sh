6fe2470348e067597b1b6eeaa38436bafa29f613
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-04 13:58:04 -0400

    fix cjson issues, make lua use readline, fix sysroot build script

--- sedna-sysroot/build.sh
+++ sedna-sysroot/build.sh
@@ -6,5 +6,6 @@ mkdir -p sysroot/usr
 cp -a ../output/staging/usr/include sysroot/usr
 cp -a ../output/staging/usr/lib sysroot/usr
 cp -a ../output/staging/lib sysroot
-rm -f sysroot/lib/*.a sysroot/usr/lib/*.a
+cp ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/crtbegin*.o ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/crtend*.o ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/libgcc.a sysroot/lib
+rm -f sysroot/usr/lib/*.a sysroot/lib/libatomic.a sysroot/lib/libc.a
 tar --owner=0 --group=0 -cJf sysroot.tar.xz sysroot
