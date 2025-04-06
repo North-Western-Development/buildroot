466c911682d78c9af8fee7f0935e9f603300dcc8
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-06 18:13:33 -0400

    fix more

--- board/sedna-riscv64/rootfs-overlay/sbin/setup-disk.sh
+++ board/sedna-riscv64/rootfs-overlay/sbin/setup-disk.sh
@@ -8,7 +8,7 @@ fi
 if [ -z "$1" ]; then
     printf 'usage: %s <drive>\n' "${0##*/}"
     exit 1
-elif ! [ -f "$1" ]; then
+elif ! [ -e "$1" ]; then
     printf 'Drive %s not found.\n' "$1"
     exit 1
 fi
