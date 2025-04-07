0e8f51d038beb06622061a48055eae60ad0ed22f
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-07 19:57:19 -0400

    remove sysroot stuff

--- sedna-sysroot/sysroot/usr/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/deck/devel/buildroot/output/build/libressl-3.9.2/include
-# Build directory: /home/deck/devel/buildroot/output/build/libressl-3.9.2/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
