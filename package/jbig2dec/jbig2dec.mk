b61673df4cc131c9963cbcf75e48e2acc55e738e
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-07 19:55:35 -0400

    remove duplicate thing

--- package/jbig2dec/jbig2dec.mk
+++ package/jbig2dec/jbig2dec.mk
@@ -15,7 +15,4 @@ JBIG2DEC_INSTALL_STAGING = YES
 # 0001-Bug-705041-jbig2dec-Avoid-uninitialized-allocator-in.patch
 JBIG2DEC_IGNORE_CVES += CVE-2023-46361
 
-# 0001-Bug-705041-jbig2dec-Avoid-uninitialized-allocator-in.patch
-JBIG2DEC_IGNORE_CVES += CVE-2023-46361
-
 $(eval $(autotools-package))
