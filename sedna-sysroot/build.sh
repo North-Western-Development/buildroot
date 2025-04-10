4b6a2ad6bd7cceeb293ccc1b504308659a2680a9
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-10 16:45:59 -0400

    rework the sysroot script slightly

--- sedna-sysroot/build.sh
+++ sedna-sysroot/build.sh
@@ -3,9 +3,8 @@ cd "$(realpath "${0%/*}")" || { printf '%s\n' "Failed to cd to the script's dire
 [ -d ../output/host ] || make -C ..
 
 mkdir -p sysroot/usr
-cp -a ../output/staging/usr/include sysroot/usr
-cp -a ../output/staging/usr/lib sysroot/usr
-cp -a ../output/staging/lib sysroot
+cp -a ../output/target/usr/include sysroot/usr
+cp -a ../output/target/usr/lib sysroot/usr
+cp -a ../output/target/lib sysroot
 cp ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/crtbegin*.o ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/crtend*.o ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/libgcc.a sysroot/lib
-rm -f sysroot/usr/lib/*.a sysroot/lib/libatomic.a sysroot/lib/libc.a
 tar --owner=0 --group=0 -cJf sysroot.tar.xz sysroot
