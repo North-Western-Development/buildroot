aa8f6016bc556181a95c5df74f04e2424eb4f5d7
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-14 00:27:07 -0400

    fix nextvi package license

--- package/nextvi/nextvi.mk
+++ package/nextvi/nextvi.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-NEXTVI_VERSION = 78a5ea269c1e1b5d18c0acc9e138476385ab1c3a
+NEXTVI_VERSION = 0fbf3712e7cda4b42b801b74f64f41c4e4327ff3
 NEXTVI_SITE = https://github.com/Un1q32/nextvi/archive
 NEXTVI_SOURCE = $(NEXTVI_VERSION).tar.gz
-NEXTVI_LICENSE = MIT
+NEXTVI_LICENSE = ISC
+NEXTVI_LICENSE_FILES = LICENSE
 
 define NEXTVI_BUILD_CMDS
 	"$(TARGET_CC)" $(TARGET_CFLAGS) -D_POSIX_C_SOURCE=200809L -flto $(@D)/vi.c -o "$(TARGET_DIR)/bin/vi"
