73ef4e7e3ca8abd7ecade8488132b971acb1e4e6
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-16 00:22:32 -0400

    fix

--- board/sedna-riscv64/post-build.sh
+++ board/sedna-riscv64/post-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm -f "$TARGET_DIR/var/lib/dpkg/info/.keep" "$TARGET_DIR/etc/bash/bashrc.d/.keep" "$TARGET_DIR/etc/resolv.conf"
+rm -f "$TARGET_DIR/var/lib/dpkg/info/.keep" "$TARGET_DIR/etc/bashrc.d/.keep" "$TARGET_DIR/etc/resolv.conf"
 cp -a "$STAGING_DIR/usr/include" "$TARGET_DIR/usr"
 cp os-release "$TARGET_DIR/usr/lib"
 sed -i '/^root/s|/bin/sh|/bin/bash|' "$TARGET_DIR/etc/passwd"
