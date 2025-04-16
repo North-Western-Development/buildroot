d9098b5ddc60128916ab182f3962fb139882f84f
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-15 23:59:56 -0400

    set default shell to bash

--- board/sedna-riscv64/post-build.sh
+++ board/sedna-riscv64/post-build.sh
@@ -2,3 +2,4 @@
 rm -f "$TARGET_DIR/var/lib/dpkg/info/.keep" "$TARGET_DIR/etc/resolv.conf"
 cp -a "$STAGING_DIR/usr/include" "$TARGET_DIR/usr"
 cp os-release "$TARGET_DIR/usr/lib"
+sed -i '/^root/s|/bin/sh|/bin/bash|' "$TARGET_DIR/etc/passwd"
