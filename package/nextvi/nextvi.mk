979bdd28e27eff75cab54f8d49e082abaa91b541
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-16 14:29:01 -0400

    add ex and view scripts for nextvi

--- package/nextvi/nextvi.mk
+++ package/nextvi/nextvi.mk
@@ -12,6 +12,9 @@ NEXTVI_LICENSE_FILES = LICENSE
 
 define NEXTVI_BUILD_CMDS
 	"$(TARGET_CC)" $(TARGET_CFLAGS) -D_POSIX_C_SOURCE=200809L -flto $(@D)/vi.c -o "$(TARGET_DIR)/bin/vi"
+	printf '#!/bin/sh\nexec /bin/vi -e "$$@"' > "$(TARGET_DIR)/bin/ex"
+	printf '#!/bin/sh\nexec /bin/vi -R "$$@"' > "$(TARGET_DIR)/bin/view"
+	chmod +x "$(TARGET_DIR)/bin/ex" "$(TARGET_DIR)/bin/view"
 endef
 
 $(eval $(generic-package))
