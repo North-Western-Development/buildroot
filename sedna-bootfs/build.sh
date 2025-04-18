cb8caa0b4190bbb52d84d22413353e8eccca3821
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-18 04:07:45 +0000

    bootfs: be more awesome

--- sedna-bootfs/build.sh
+++ sedna-bootfs/build.sh
@@ -17,9 +17,6 @@ make -j"$(nproc)"
 
 mkdir -p bootfs/bin bootfs/proc bootfs/dev/pts bootfs/sys bootfs/mnt/scratch bootfs/mnt/builtin bootfs/mnt/upper bootfs/mnt/lower bootfs/sbin bootfs/newroot
 
-cp busyboxsrc/busybox bootfs/bin/busybox
-for applet in sh mkdir mount umount chroot; do
-    ln -s busybox "bootfs/bin/$applet"
-done
+cp busyboxsrc/busybox bootfs/bin
 cp init bootfs/sbin
 mksquashfs bootfs bootfs.squashfs -all-root -comp zstd
