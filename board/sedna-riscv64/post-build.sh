1cceb2cb2404dca3de2a3be7596728cd095e6e8d
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-18 02:47:15 +0000

    go back to busybox shell, remove dash, dont use bash as default interactive shell, new profile with colors

--- board/sedna-riscv64/post-build.sh
+++ board/sedna-riscv64/post-build.sh
@@ -2,4 +2,3 @@
 rm -f "$TARGET_DIR/var/lib/dpkg/info/.keep" "$TARGET_DIR/etc/bashrc.d/.keep" "$TARGET_DIR/etc/resolv.conf"
 cp -a "$STAGING_DIR/usr/include" "$TARGET_DIR/usr"
 cp os-release "$TARGET_DIR/usr/lib"
-sed -i '/^root/s|/bin/dash|/bin/bash|' "$TARGET_DIR/etc/passwd"
