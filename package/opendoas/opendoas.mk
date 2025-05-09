3dafced686fb9a77a5dd58428769a0b018d7d32a
Un1q32 <joey.t.reinhart@gmail.com>
2025-05-09 14:42:25 -0400

    opendoas: allow root to use doas with no password, add simple password prompt patch

--- package/opendoas/opendoas.mk
+++ package/opendoas/opendoas.mk
@@ -27,7 +27,7 @@ endef
 
 define OPENDOAS_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR="$(TARGET_DIR)" -C $(@D) install
-	printf 'permit persist :wheel\n' > "$(TARGET_DIR)/etc/doas.conf"
+	printf 'permit nopass 0\npermit persist :wheel\n' > "$(TARGET_DIR)/etc/doas.conf"
 	chmod 640 "$(TARGET_DIR)/etc/doas.conf"
 endef
 
