8d4779627cf1f0f046d2bc9ebaa0d88a7324b6cc
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-09 12:24:58 -0400

    add apt

---
+++ package/apt/apt.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# apt
+#
+################################################################################
+
+APT_VERSION = 3.1.3
+APT_SITE = https://salsa.debian.org/apt-team/apt/-/archive/$(APT_VERSION)
+APT_SOURCE = apt-$(APT_VERSION).tar.bz2
+APT_LICENSE = ISC
+APT_LICENSE_FILES = COPYING
+APT_DEPENDENCIES = openssl berkeleydb zlib xz bzip2 lz4 xxhash
+
+APT_CONF_OPTS = -DWITH_DOC=OFF -DWITH_TESTS=OFF
+
+ifeq ($(BR2_SYSTEM_ENABLE_NLS),y)
+APT_CONF_OPTS += -DUSE_NLS=ON
+else
+APT_CONF_OPTS += -DUSE_NLS=OFF
+endif
+
+$(eval $(cmake-package))
