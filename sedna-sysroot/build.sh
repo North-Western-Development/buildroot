5761ff7bfb5998e8b45ea983769ac6f0f328e186
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-09 21:42:55 -0400

    add cmake and pkgconfig files to sysroot

--- sedna-sysroot/build.sh
+++ sedna-sysroot/build.sh
@@ -3,8 +3,10 @@ cd "$(realpath "${0%/*}")" || { printf '%s\n' "Failed to cd to the script's dire
 [ -d ../output/host ] || make -C ..
 
 mkdir -p sysroot/usr
-cp -a ../output/target/usr/include sysroot/usr
-cp -a ../output/target/usr/lib sysroot/usr
+cp -a ../output/target/usr/include ../output/target/usr/lib sysroot/usr
 cp -a ../output/target/lib sysroot
-cp ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/crtbegin*.o ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/crtend*.o ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/libgcc.a sysroot/lib
+cp -a ../output/staging/usr/lib/pkgconfig ../output/staging/usr/lib/cmake sysroot/usr/lib
+cp ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/crtbegin*.o \
+    ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/crtend*.o \
+    ../output/host/lib/gcc/riscv64-buildroot-linux-musl/*/libgcc.a sysroot/lib
 tar --owner=0 --group=0 -cJf sysroot.tar.xz sysroot
