7c121db52e051005c8f2b1c29240644910cf56c1
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-10 17:19:26 -0400

    update tinycc

--- package/tinycc/tinycc.mk
+++ package/tinycc/tinycc.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TINYCC_VERSION = f10ab130ecbefc99177f5569c69ebe7f375a3d44
+TINYCC_VERSION = fa9795406d4f6579d95e1333bde27cf437023fe4
 TINYCC_SITE = https://repo.or.cz/tinycc.git/snapshot
 TINYCC_SOURCE = $(TINYCC_VERSION).tar.gz
 TINYCC_LICENSE = LGPL-2.1
