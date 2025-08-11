8eff40771b8591b9a240406dcbea25ac1a7b4980
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-11 12:44:41 -0400

    remove apt stuff

--- board/sedna-riscv64/post-build.sh
+++ board/sedna-riscv64/post-build.sh
@@ -2,4 +2,3 @@
 rm -f "$TARGET_DIR/etc/bashrc.d/.keep" "$TARGET_DIR/etc/resolv.conf" "$TARGET_DIR/home/.keep"
 cp -a "$STAGING_DIR/usr/include" "$TARGET_DIR/usr"
 cp os-release "$TARGET_DIR/usr/lib"
-printf '%s\n' '_apt:x:420:420:APT account for owning persistent & cache data:/var/lib/apt:/usr/sbin/nologin' >> "$TARGET_DIR/etc/passwd"
