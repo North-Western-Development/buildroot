2b5660ad6520117bc98509dfb550703b0934ce5a
Un1q32 <joey.t.reinhart@gmail.com>
2025-08-18 15:42:59 -0400

    re-enable apt

--- board/sedna-riscv64/post-build.sh
+++ board/sedna-riscv64/post-build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-rm -f "$TARGET_DIR/var/lib/dpkg/info/.keep" "$TARGET_DIR/etc/bashrc.d/.keep" "$TARGET_DIR/etc/resolv.conf" "$TARGET_DIR/home/.keep"
+rm -f "$TARGET_DIR/etc/bashrc.d/.keep" "$TARGET_DIR/etc/resolv.conf" "$TARGET_DIR/home/.keep"
 cp -a "$STAGING_DIR/usr/include" "$TARGET_DIR/usr"
 cp os-release "$TARGET_DIR/usr/lib"
+printf '%s\n' '_apt:x:420:420:APT account for owning persistent & cache data:/var/lib/apt:/usr/sbin/nologin' >> "$TARGET_DIR/etc/passwd"
