399d2a99a364192aaecdb223f933ce93ac739507
Un1q32 <joey.t.reinhart@gmail.com>
2025-10-02 19:22:04 -0400

    update nextvi

--- package/nextvi/nextvi.mk
+++ package/nextvi/nextvi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NEXTVI_VERSION = 8b24eb0b74f216e2796cf7bd347a4cd671a89745
+NEXTVI_VERSION = 95561c04b024ec80de5c29535b49a27e4a02ee8d
 NEXTVI_SITE = https://github.com/Un1q32/nextvi/archive
 NEXTVI_SOURCE = $(NEXTVI_VERSION).tar.gz
 NEXTVI_LICENSE = ISC
