e42547a1a5c3716fb015c6b92fcbb1d4deb9472b
Daniel Houck <Software@DRHouck.me>
2026-02-17 06:01:48 -0500

    Update MICROPYPATH to still include installed libraries
    Things worked before because Micropython has built-in equivalents for
    all the libraries, we use, but the ones installed in /usr/lib are
    usually better.

--- board/sedna-riscv64/rootfs-overlay/etc/profile.d/python_path.sh
+++ board/sedna-riscv64/rootfs-overlay/etc/profile.d/python_path.sh
@@ -1,5 +1,7 @@
 if [ -z "$MICROPYPATH" ]; then
-    export MICROPYPATH="/mnt/builtin/lib/micropython"
+    # MICROPYPATH, unlike PYTHONPATH, overrides the whole path; we need to
+    # append the default value (from ports/unix/mpconfigport.h)
+    export MICROPYPATH="/mnt/builtin/lib/micropython:.frozen:~/.micropython/lib:/usr/lib/micropython"
 else
     export MICROPYPATH="$MICROPYPATH:/mnt/builtin/lib/micropython"
 fi
